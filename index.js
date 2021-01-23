fe0095f46db65ba3ec730dfe559d9896df4ef048
Nagaraju <balanagaraju2506@gmail.com>
2021-01-23 12:03:42 +0530

    hiii

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
+const bodyParser = require('body-parser');
 var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 const PORT=process.env.PORT || 3000;
 app.get('/', function(req, res){
    res.send("Hello world!");
@@ -11,14 +13,18 @@ app.get('/data', function(req, res){
     res.send("Hello data1!");
  })  ;
  app.get('/babay', function(req, res){
-    res.send("Hello babayyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy yyyyyyyyyy yenti sangathuluuuu!");
+    res.send("Hello babayyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy yyyyyyyyyy yenti sangathuluuuu!");
  });
+ var x=0;
  app.post('/hehe',(req,res)=>{
-    //  console.log(req)
-    //  res.send("hiiiiiiiiiiii")
+    x=x+1;
+     console.log(req.body,"heehhhehhehrhiii hiii hii",x)
+   //   res.send("hiiiiiiiiiiii")
      app.get('/hehe',(req1,res1)=>{
-         res1.send("babayyy hehhehhehheheehheheheehehehhehehehehheheeheheh")
+        console.log(x)
+        y={a:x}
+         res1.send(y)
      })
  })
- 
+   
 app.listen(PORT);
