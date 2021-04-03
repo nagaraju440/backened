42fb1cdcd74d48712d83190de23000e20fbe465f
Nagaraju <balanagaraju2506@gmail.com>
2021-04-03 12:33:08 +0530

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,81 @@
 var express = require('express');
 const bodyParser = require('body-parser');
+const { graphqlExpress, graphiqlExpress,ApolloServer, gql } = require('apollo-server-express');
+const { makeExecutableSchema } = require('graphql-tools');
+const mongoose = require('mongoose');
+var Schema=mongoose.Schema;
 var app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
+var user;
+const cors = require('cors');
+app.use(cors())
+app.use(bodyParser.json());
+const url="mongodb+srv://balanagaraju:Nagaraju2506@cluster0.xka5z.mongodb.net/example?retryWrites=true&w=majority"
+mongoose.connect(url,{useNewUrlParser: true})
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open',function(){
+  console.log("data connected sucsessfully")
+});
+var ex=new Schema({
+  name:String,
+})
+var ex1=mongoose.model('ex3',ex)
+var ex2=new ex1({name:"ganesh"})
+
+ex2.save((err,res)=>{
+  console.log("name saved sucsessfully")
+})
+app.post('/users',(req,res)=>{
+  //  var x=eval(req.body);
+  console.log("sucsessfull")
+  //  console.log(req.body.data,typeof(req.body.data),"this is my datar")
+    user=req.body.data;
+app.get('/users',(req1,res1)=>{
+  res1.send(req.body.data)
+      })
+    })
+const books = [
+   {
+     title: "Harry Potter and the Sorcerer's stone",
+     author: 'J.K. Rowling',
+     hi:"hi1",
+   },
+   {
+     title: 'Jurassic Park',
+     author: 'Michael Crichton',
+     hi:"hi2",
+   },
+ ];
+ const books1 = [
+  {
+    title: "praveen",
+    author: 'html',
+    hi:"hi andi nenu praveen",
+  },
+  {
+    title: 'Hanuma',
+    author: 'css',
+    hi:"hi andi nenu hanuma",
+  },
+];
+ const typeDefs = `
+  type Query { books: [Book],books1:[Book1],user:[Users],ex2:[ex2] }
+  type Book { title: String, author: String,hi:String }
+  type Book1{ title: String, author: String,hi:String }
+  type Users{userId:Int,id:Int,title:String,body:String}
+  type ex2{name:String}
+`;
+
+// The resolvers
+const resolvers = {
+  Query: { books: () => books,books1:()=>books1 ,user:()=>user,ex2:()=>{return ex1.find({},(req,res)=>{console.log(res,"is res")})}},
+};
+
+// Put together a schema
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
 const PORT=process.env.PORT || 3000;
 app.get('/', function(req, res){
    res.send("Hello world!");
@@ -10,12 +84,13 @@ app.get('/data', function(req, res){
     res.send("Hello data!");
  });
  app.get('/data1', function(req, res){
-    res.send("Hello data1!");
+    res.send("Hello data1hvjebk!");
  })  ;
  app.get('/babay', function(req, res){
     res.send("Hello babayyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy yyyyyyyyyy yenti sangathuluuuu!");
  });
- var x=0;
+ 
+     var x=0;
  app.post('/hehe',(req,res)=>{
     x=x+1;
      console.log(req.body,"heehhhehhehrhiii hiii hii",x)
@@ -26,5 +101,8 @@ app.get('/data', function(req, res){
          res1.send(y)
      })
  })
-   
+ const server = new ApolloServer({ typeDefs, resolvers });
+ server.applyMiddleware({ app });
 app.listen(PORT);
+
+
